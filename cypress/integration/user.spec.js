e776ce6806eedd06ba050489077fe1aa3296ff69
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-12 21:45:53 -0300

    feat(integration - user.spec.js):email already exists

--- cypress/integration/user.spec.js
+++ cypress/integration/user.spec.js
@@ -2,8 +2,10 @@
 
 describe('User', () => {
 
-  it('createUser', () => {
-    cy.visit('http://localhost:3000/');
+  const baseUrl = 'http://localhost:3000/';
+
+  it.skip('createUser', () => {
+    cy.visit(baseUrl);
 
     cy.get('.fDRVEk').click()
 
@@ -26,7 +28,67 @@ describe('User', () => {
     })
   });
 
-  it('logout', () => {
+
+  it('emailAlreadyExists', () => {
+    cy.visit(baseUrl);
+
+    cy.get('.fDRVEk').click()
+
+    cy.get(':nth-child(1) > .sc-jcFjpl > input').type('testeCypress1');
+
+
+    cy.get(':nth-child(2) > .sc-jcFjpl > input').type('luby@admin.com.com');
+
+    cy.get(':nth-child(3) > .sc-jcFjpl > input').type('123456');
+
+
+    cy.intercept('POST', '**/user/create').as('createUser');
+
+    cy.get('.sc-kDTinF > .sc-bdvvtL').click();
+
+    cy.wait('@createUser').then(({ response }) => {
+      expect(response.statusCode).be.eq(400);
+      expect(response.body).has.property('error');
+      expect(response.body.error).is.not.null;
+    })
+  });
+
+
+  it.skip('emailInvalid', () => {
+    cy.visit(baseUrl);
+
+    cy.get('.fDRVEk').click()
+
+    cy.get(':nth-child(1) > .sc-jcFjpl > input').type('testeCypress1');
+
+
+    cy.get(':nth-child(2) > .sc-jcFjpl > input').type('teste6s6gmail.com');
+
+    cy.get(':nth-child(3) > .sc-jcFjpl > input').type('123456');
+
+    cy.get('.sc-kDTinF > .sc-bdvvtL').click();
+
+  });
+
+
+  it.skip('passwordInvalid', () => {
+    cy.visit(baseUrl);
+
+    cy.get('.fDRVEk').click()
+
+    cy.get(':nth-child(1) > .sc-jcFjpl > input').type('testeCypress1');
+
+
+    cy.get(':nth-child(2) > .sc-jcFjpl > input').type('teste@gmail.com');
+
+    cy.get(':nth-child(3) > .sc-jcFjpl > input').type('12345');
+
+    cy.get('.sc-kDTinF > .sc-bdvvtL').click();
+
+  });
+
+
+  it.skip('logout', () => {
     cy.get('.sc-jgrJph > .sc-bdvvtL').click()
   });
 
