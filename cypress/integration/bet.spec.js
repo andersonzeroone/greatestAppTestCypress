c9a7b735ac9d06523fcd4c97b417ed4311405b4a
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-12 23:01:13 -0300

    feat(integration - bet.spec.js): creating add-to-cart tests

--- cypress/integration/bet.spec.js
+++ cypress/integration/bet.spec.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress"/>
 
 
-describe('BetsFilter', () => {
+describe('Bets', () => {
 
   const baseUrl = 'http://localhost:3000/'
 
-  it('betFiltered', () => {
+  it.skip('betFiltered', () => {
     cy.visit(baseUrl);
 
     cy.get(':nth-child(1) >> input').type('luby@admin.com');
@@ -25,6 +25,55 @@ describe('BetsFilter', () => {
     cy.get('[data-value="1"] > .e-chips-close').click();
 
   });
+
+
+  it.skip('addBetsInCart', () => {
+    cy.visit(baseUrl);
+
+    cy.get(':nth-child(1) >> input').type('luby@admin.com');
+
+    cy.get(':nth-child(2) >> input').type('secret');
+
+    cy.get('.sc-kDTinF > .sc-bdvvtL').click();
+
+    cy.get('.sc-gIDmLj > .sc-bdvvtL').click();
+
+    cy.get('.sc-jWUzzU > :nth-child(1)').click();
+
+    cy.get('.sc-lkgTHX').click();
+
+    cy.get('.cNojwj').click();
+
+    cy.get('.sc-lkgTHX').click();
+
+
+    cy.get('.sc-jWUzzU > :nth-child(1)').click();
+    cy.get('.sc-lkgTHX').click();
+
+    cy.get('.sc-cxpSdN').click();
+
+  });
+
+  it('noAddBetsInCartValueMin', () => {
+    cy.visit(baseUrl);
+
+    cy.get(':nth-child(1) >> input').type('luby@admin.com');
+
+    cy.get(':nth-child(2) >> input').type('secret');
+
+    cy.get('.sc-kDTinF > .sc-bdvvtL').click();
+
+    cy.get('.sc-gIDmLj > .sc-bdvvtL').click();
+
+    cy.get('.sc-jWUzzU > :nth-child(1)').click();
+
+    cy.get('.sc-lkgTHX').click();
+
+    cy.get('.sc-cxpSdN > .sc-bdvvtL').click()
+
+
+  });
+
 
 
 });
